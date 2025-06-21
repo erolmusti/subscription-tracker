696a768c7f929b4d08e18aa8df59d2d84adf17e3
erolmusti <100385200+erolmusti@users.noreply.github.com>
2025-06-21 22:33:48 +0300

    Fix Expo Metro Configuration and Dependencies

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Ensure Metro can resolve all necessary modules
-config.resolver.platforms = ['native', 'ios', 'android', 'web'];
-
 module.exports = config;
