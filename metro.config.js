2518c7f2a3e17b9d53808a6528b39cf479a4b7fd
erolmusti <100385200+erolmusti@users.noreply.github.com>
2025-06-21 23:34:37 +0300

    Added package-lock.json

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,15 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Enable web support
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Add support for additional asset extensions
+config.resolver.assetExts.push(
+  // Adds support for `.db` files for SQLite databases
+  'db'
+);
 
 module.exports = config;
